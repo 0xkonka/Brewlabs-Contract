01e5bfe154859b89b260900e6fb45c20cf0521d1
brainstormk <mverse@brewlabs.info>
2022-12-07 22:17:24 +0800

    Create MockErc721.sol

---
+++ contracts/mocks/MockErc721.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MockErc721 is ERC721 {
+    uint256 public supply;
+
+    constructor() ERC721("Test Nft", "TNFT") {}
+
+    function mint(address _to) external {
+        supply++;
+        _safeMint(_to, supply);
+    }
+
+    function burn(uint256 _tokenId) external {
+        _burn(_tokenId);
+    }
+}
