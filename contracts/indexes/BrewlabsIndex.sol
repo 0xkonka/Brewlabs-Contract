d3a9f949bd5e3ee9ded988a61824e4629919601b
brainstormk <109654392+brainstormk@users.noreply.github.com>
2023-03-21 22:40:50 +0800

    Rename brewlabs index contracts

---
+++ contracts/indexes/BrewlabsIndex.sol
@@ -0,0 +1,437 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import { IERC20, SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import { ERC721Holder } from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+
+import "../libs/IUniRouter02.sol";
+
+interface IBrewlabsIndexNft is IERC721 {
+  function mint(address to) external returns (uint256);
+
+  function burn(uint256 tokenId) external;
+}
+
+// BrewlabsIndex is index contracts that offer a range of token collections to buy as "Brewlabs Index"
+// most likely top 100 tokens that do not require tax slippage.
+// Ideally the index tokens will buy 2-4 tokens (they will mostly be pegged tokens of the top 100 tokens that we will choose).
+//
+// Note User may select an index that will contain PEGGED-ETH + BTCB,
+// the will determine how much (by a sliding scale) BNB they will allocate to each token.
+// For example 1 BNB buy:
+//    User chooses 30%; 0.30 BNB to buy PEGGED-ETH (BEP20)
+//    User chooses 70%, 0.70BNB to buy BTCB.
+contract BrewlabsIndex is Ownable, ERC721Holder, ReentrancyGuard {
+  using SafeERC20 for IERC20;
+
+  // Whether it is initialized
+  bool public isInitialized;
+
+  uint256 private PERCENTAGE_PRECISION;
+
+  IERC721 public nft;
+  IERC20[] public tokens;
+  uint256 public NUM_TOKENS;
+
+  address public swapRouter;
+  address[][] public ethToTokenPaths;
+
+  // Info of each user.
+  struct UserInfo {
+    uint256[] amounts; // How many tokens that user has bought
+    uint256 zappedEthAmount; // ETH amount that user sold
+  }
+
+  mapping(address => UserInfo) private users;
+
+  struct NftInfo {
+    uint256[] amounts; // locked token amounts in NFT
+    uint256 zappedEthAmount; // ETH amount that sold for above tokens
+  }
+
+  mapping(uint256 => NftInfo) private nfts;
+  uint256[] public totalStaked;
+
+  uint256 public fee;
+  uint256 public performanceFee;
+  address public treasury;
+
+  event TokenZappedIn(
+    address indexed user,
+    uint256 ethAmount,
+    uint256[] percents,
+    uint256[] amountOuts
+  );
+  event TokenZappedOut(address indexed user, uint256 ethAmount, uint256[] amounts);
+  event TokenClaimed(address indexed user, uint256[] amounts);
+  event TokenLocked(address indexed user, uint256[] amounts, uint256 ethAmount, uint256 tokenId);
+  event TokenUnLocked(address indexed user, uint256[] amounts, uint256 ethAmount, uint256 tokenId);
+
+  event ServiceInfoUpadted(address addr, uint256 fee);
+  event SetFee(uint256 fee);
+  event SetSettings(address router, address[][] paths);
+
+  modifier onlyInitialized() {
+    require(isInitialized, "Not initialized");
+    _;
+  }
+
+  constructor() {}
+
+  /**
+   * @notice Initialize indexes contract.
+   * @param _tokens: token list that user can buy in a transaction
+   * @param _nft: NFT contract address for locking tokens
+   * @param _router: swap router address
+   * @param _paths: swap paths for each token
+   */
+  function initialize(
+    IERC20[] memory _tokens,
+    IERC721 _nft,
+    address _router,
+    address[][] memory _paths,
+    address _owner
+  ) external {
+    require(!isInitialized, "Already initialized");
+    require(owner() == address(0x0) || msg.sender == owner(), "Not allowed");
+
+    isInitialized = true;
+
+    // initialize default variables
+    PERCENTAGE_PRECISION = 10000;
+    NUM_TOKENS = tokens.length;
+
+    fee = 25;
+    performanceFee = 0.01 ether;
+    treasury = 0x5Ac58191F3BBDF6D037C6C6201aDC9F99c93C53A;
+
+    nft = _nft;
+    tokens = _tokens;
+    swapRouter = _router;
+    ethToTokenPaths = _paths;
+
+    _transferOwnership(_owner);
+  }
+
+  /**
+   * @notice Buy tokens by paying ETH and lock tokens in contract.
+   *         When buy tokens, should pay performance fee and processing fee.
+   * @param _percents: list of ETH allocation points to buy tokens
+   */
+  function zapIn(uint256[] memory _percents) external payable onlyInitialized nonReentrant {
+    uint256 totalPercentage = 0;
+    for (uint8 i = 0; i < NUM_TOKENS; i++) {
+      totalPercentage += _percents[i];
+    }
+    require(totalPercentage <= PERCENTAGE_PRECISION, "Total percentage cannot exceed 10000");
+
+    uint256 ethAmount = msg.value;
+
+    // pay processing fee
+    uint256 buyingFee = (ethAmount * fee) / PERCENTAGE_PRECISION;
+    payable(treasury).transfer(buyingFee);
+    ethAmount -= buyingFee;
+
+    UserInfo storage user = users[msg.sender];
+
+    // buy tokens
+    uint256 amount;
+    uint256[] memory amountOuts;
+    for (uint8 i = 0; i < NUM_TOKENS; i++) {
+      uint256 amountIn = (ethAmount * _percents[i]) / PERCENTAGE_PRECISION;
+      if (amountIn == 0) continue;
+
+      amountOuts[i] = _safeSwapEth(amountIn, getSwapPath(i, true), address(this));
+
+      user.amounts[i] += amountOuts[i];
+      amount += amountIn;
+
+      totalStaked[i] += amountOuts[i];
+    }
+    user.zappedEthAmount += amount;
+
+    emit TokenZappedIn(msg.sender, amount, _percents, amountOuts);
+  }
+
+  /**
+   * @notice Claim tokens from contract.
+   *         If the user exits the index in a loss then there is no fee.
+   *         If the user exists the index in a profit, processing fee will be applied.
+   */
+  function claimTokens() external onlyInitialized nonReentrant {
+    UserInfo memory user = users[msg.sender];
+    require(user.zappedEthAmount > 0, "No available tokens");
+
+    uint256 expectedAmt = _expectedEth(user.amounts);
+    if (expectedAmt > user.zappedEthAmount) {
+      for (uint8 i = 0; i < NUM_TOKENS; i++) {
+        uint256 claimFee = (user.amounts[i] * fee) / PERCENTAGE_PRECISION;
+        tokens[i].safeTransfer(treasury, claimFee);
+        tokens[i].safeTransfer(msg.sender, user.amounts[i] - claimFee);
+      }
+    } else {
+      for (uint8 i = 0; i < NUM_TOKENS; i++) {
+        totalStaked[i] -= user.amounts[i];
+        tokens[i].safeTransfer(msg.sender, user.amounts[i]);
+      }
+    }
+
+    emit TokenClaimed(msg.sender, user.amounts);
+    delete users[msg.sender];
+  }
+
+  /**
+   * @notice Sale tokens from contract and claim ETH.
+   *         If the user exits the index in a loss then there is no fee.
+   *         If the user exists the index in a profit, processing fee will be applied.
+   */
+  function zapOut() external onlyInitialized nonReentrant {
+    UserInfo memory user = users[msg.sender];
+    require(user.zappedEthAmount > 0, "No available tokens");
+
+    uint256 ethAmount;
+    for (uint8 i = 0; i < NUM_TOKENS; i++) {
+      totalStaked[i] -= user.amounts[i];
+      uint256 amountOut = _safeSwapForETH(user.amounts[i], getSwapPath(i, false));
+      ethAmount += amountOut;
+    }
+
+    if (ethAmount > user.zappedEthAmount) {
+      uint256 swapFee = (ethAmount * fee) / PERCENTAGE_PRECISION;
+      payable(treasury).transfer(swapFee);
+
+      ethAmount -= swapFee;
+    }
+
+    payable(msg.sender).transfer(ethAmount);
+    emit TokenZappedOut(msg.sender, ethAmount, user.amounts);
+    delete users[msg.sender];
+  }
+
+  /**
+   * @notice Once the user purchases the tokens through the contract, the user can then choose to at anytime
+   *  to mint an NFT that would represent the ownership of their tokens in the contract.
+   * The purpose of this is to allow users to mint an NFT that represents their value in the index and at their discretion,
+   *  transfer or sell that NFT to another wallet.
+   */
+  function mintNft() external payable onlyInitialized nonReentrant returns (uint256) {
+    UserInfo storage user = users[msg.sender];
+    require(user.zappedEthAmount > 0, "No available tokens");
+
+    _transferPerformanceFee();
+
+    // mint NFT
+    uint256 tokenId = IBrewlabsIndexNft(address(nft)).mint(msg.sender);
+
+    // lock available tokens for NFT
+    NftInfo storage nftData = nfts[tokenId];
+    nftData.amounts = user.amounts;
+    nftData.zappedEthAmount = user.zappedEthAmount;
+
+    delete users[msg.sender];
+    emit TokenLocked(msg.sender, nftData.amounts, nftData.zappedEthAmount, tokenId);
+
+    return tokenId;
+  }
+
+  /**
+   * @notice Stake the NFT back into the index to claim/zap out their tokens.
+   */
+  function stakeNft(uint256 tokenId) external payable onlyInitialized nonReentrant {
+    UserInfo storage user = users[msg.sender];
+
+    _transferPerformanceFee();
+
+    // burn NFT
+    nft.safeTransferFrom(msg.sender, address(this), tokenId);
+    IBrewlabsIndexNft(address(nft)).burn(tokenId);
+
+    NftInfo memory nftData = nfts[tokenId];
+    for (uint8 i = 0; i < NUM_TOKENS; i++) {
+      user.amounts[i] += nftData.amounts[i];
+    }
+    user.zappedEthAmount = nftData.zappedEthAmount;
+
+    emit TokenUnLocked(msg.sender, nftData.amounts, nftData.zappedEthAmount, tokenId);
+    delete nfts[tokenId];
+  }
+
+  /**
+   * @notice Returns purchased tokens and ETH amount at the time when bought tokens.
+   * @param _user: user address
+   */
+  function userInfo(
+    address _user
+  ) external view returns (uint256[] memory amounts, uint256 ethAmount) {
+    UserInfo memory _userData = users[_user];
+    ethAmount = _userData.zappedEthAmount;
+    amounts = new uint256[](NUM_TOKENS);
+    for (uint256 i = 0; i < NUM_TOKENS; i++) {
+      amounts[i] = _userData.amounts[i];
+    }
+  }
+
+  /**
+   * @notice Returns tokens locked in NFT and ETH amount at the time when bought tokens.
+   * @param _tokenId: owned tokenId
+   */
+  function nftInfo(
+    uint256 _tokenId
+  ) external view returns (uint256[] memory amounts, uint256 ethAmount) {
+    NftInfo memory _nftData = nfts[_tokenId];
+    ethAmount = _nftData.zappedEthAmount;
+    amounts = new uint256[](NUM_TOKENS);
+    for (uint256 i = 0; i < NUM_TOKENS; i++) {
+      amounts[i] = _nftData.amounts[i];
+    }
+  }
+
+  /**
+   * @notice Returns swap path for token specified by index.
+   * @param _index: token index
+   * @param _isZapIn: swap direction(true: ETH to token, false: token to ETH)
+   */
+  function getSwapPath(uint8 _index, bool _isZapIn) public view returns (address[] memory) {
+    uint256 len = ethToTokenPaths[_index].length;
+    address[] memory _path = new address[](len);
+    for (uint8 j = 0; j < len; j++) {
+      if (_isZapIn) {
+        _path[j] = ethToTokenPaths[_index][j];
+      } else {
+        _path[j] = ethToTokenPaths[_index][len - j - 1];
+      }
+    }
+
+    return _path;
+  }
+
+  /**
+   * @notice Update swap router and paths for each token.
+   * @param _router: swap router address
+   * @param _paths: list of swap path for each tokens
+   */
+  function setSwapSettings(
+    address _router,
+    address[][] memory _paths
+  ) external onlyOwner onlyInitialized {
+    require(_router != address(0x0), "Invalid address");
+    require(IUniRouter02(_router).WETH() != address(0x0), "Invalid swap router");
+
+    swapRouter = _router;
+    ethToTokenPaths = _paths;
+    emit SetSettings(_router, _paths);
+  }
+
+  /**
+   * @notice Update processing fee.
+   * @param _fee: percentage in point
+   */
+  function setFee(uint256 _fee) external onlyOwner {
+    require(_fee <= PERCENTAGE_PRECISION, "Invalid percentage");
+    fee = _fee;
+    emit SetFee(_fee);
+  }
+
+  /**
+   * This method can be called by treasury.
+   * @notice Update treasury wallet and performance fee.
+   * @param _addr: new treasury address
+   * @param _fee: percentage in point
+   */
+  function setServiceInfo(address _addr, uint256 _fee) external {
+    require(msg.sender == treasury, "setServiceInfo: FORBIDDEN");
+    require(_addr != address(0x0), "Invalid address");
+
+    treasury = _addr;
+    performanceFee = _fee;
+
+    emit ServiceInfoUpadted(_addr, _fee);
+  }
+
+  /**
+   * @notice Emergency withdraw tokens.
+   * @param _token: token address
+   */
+  function rescueTokens(address _token) external onlyOwner {
+    if (_token == address(0x0)) {
+      uint256 _ethAmount = address(this).balance;
+      payable(msg.sender).transfer(_ethAmount);
+    } else {
+      uint256 _tokenAmount = IERC20(_token).balanceOf(address(this));
+      IERC20(_token).safeTransfer(msg.sender, _tokenAmount);
+    }
+  }
+
+  /**
+   * @notice Process performance fee.
+   */
+  function _transferPerformanceFee() internal {
+    require(msg.value >= performanceFee, "Should pay small gas to call method");
+    payable(treasury).transfer(performanceFee);
+  }
+
+  /**
+   * @notice Returns the expected eth amount by swapping provided tokens.
+   * @param amounts: amounts to swap
+   */
+  function _expectedEth(uint256[] memory amounts) internal view returns (uint256 amountOut) {
+    amountOut = 0;
+    for (uint8 i = 0; i < NUM_TOKENS; i++) {
+      if (amounts[i] == 0) continue;
+      uint256[] memory _amounts = IUniRouter02(swapRouter).getAmountsOut(
+        amounts[i],
+        getSwapPath(i, false)
+      );
+      amountOut += _amounts[_amounts.length - 1];
+    }
+  }
+
+  /**
+   * @notice get token from ETH via swap.
+   * @param _amountIn: eth amount to swap
+   * @param _path: swap path
+   * @param _to: receiver address
+   */
+  function _safeSwapEth(
+    uint256 _amountIn,
+    address[] memory _path,
+    address _to
+  ) internal returns (uint256) {
+    address _token = _path[_path.length - 1];
+    uint256 beforeAmt = IERC20(_token).balanceOf(address(this));
+    IUniRouter02(swapRouter).swapExactETHForTokensSupportingFeeOnTransferTokens{ value: _amountIn }(
+      0,
+      _path,
+      _to,
+      block.timestamp + 600
+    );
+    uint256 afterAmt = IERC20(_token).balanceOf(address(this));
+
+    return afterAmt - beforeAmt;
+  }
+
+  /**
+   * @notice swap tokens to ETH.
+   * @param _amountIn: token amount to swap
+   * @param _path: swap path
+   */
+  function _safeSwapForETH(uint256 _amountIn, address[] memory _path) internal returns (uint256) {
+    IERC20(_path[0]).safeApprove(swapRouter, _amountIn);
+
+    uint256 beforeAmt = address(this).balance;
+    IUniRouter02(swapRouter).swapExactTokensForETHSupportingFeeOnTransferTokens(
+      _amountIn,
+      0,
+      _path,
+      address(this),
+      block.timestamp + 600
+    );
+
+    return address(this).balance - beforeAmt;
+  }
+
+  receive() external payable {}
+}
---
+++ contracts/indexes/BrewlabsIndexNft.sol
@@ -0,0 +1,260 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC721, ERC721Enumerable, IERC721 } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+import { DefaultOperatorFilterer } from "operator-filter-registry/src/DefaultOperatorFilterer.sol";
+
+interface IBrewlabsIndex {
+  function NUM_TOKENS() external view returns (uint8);
+
+  function tokens(uint256 index) external view returns (address);
+
+  function nftInfo(uint256 _tokenId) external view returns (uint256[] memory, uint256);
+}
+
+contract BrewlabsIndexNft is ERC721Enumerable, DefaultOperatorFilterer, Ownable {
+  using Strings for uint256;
+  using Strings for address;
+
+  uint256 private tokenIndex;
+  string private _tokenBaseURI = "";
+  mapping(uint256 => string) private _tokenURIs;
+
+  address public admin;
+  mapping(address => bool) public isMinter;
+  mapping(uint256 => address) private indexes;
+
+  event BaseURIUpdated(string uri);
+  event SetAdminRole(address newAdmin);
+  event SetMinterRole(address minter, bool status);
+
+  modifier onlyMinter() {
+    require(isMinter[msg.sender], "BrewlabsIndexNft: Caller is not minter");
+    _;
+  }
+
+  modifier onlyAdmin() {
+    require(
+      msg.sender == owner() || msg.sender == admin,
+      "BrewlabsIndexNft: Caller is not admin or owner"
+    );
+    _;
+  }
+
+  constructor() ERC721("Brewlabs Index Nft", "BINDEX") {
+    admin = msg.sender;
+  }
+
+  function mint(address to) external onlyMinter returns (uint256) {
+    tokenIndex++;
+    _safeMint(to, tokenIndex);
+    _setTokenURI(tokenIndex, tokenIndex.toString());
+
+    indexes[tokenIndex] = msg.sender;
+    return tokenIndex;
+  }
+
+  function burn(uint256 tokenId) external {
+    _burn(tokenId);
+  }
+
+  function setApprovalForAll(
+    address operator,
+    bool approved
+  ) public override(ERC721, IERC721) onlyAllowedOperatorApproval(operator) {
+    super.setApprovalForAll(operator, approved);
+  }
+
+  function approve(
+    address operator,
+    uint256 tokenId
+  ) public override(ERC721, IERC721) onlyAllowedOperatorApproval(operator) {
+    super.approve(operator, tokenId);
+  }
+
+  function transferFrom(
+    address from,
+    address to,
+    uint256 tokenId
+  ) public override(ERC721, IERC721) onlyAllowedOperator(from) {
+    super.transferFrom(from, to, tokenId);
+  }
+
+  function safeTransferFrom(
+    address from,
+    address to,
+    uint256 tokenId
+  ) public override(ERC721, IERC721) onlyAllowedOperator(from) {
+    super.safeTransferFrom(from, to, tokenId);
+  }
+
+  function safeTransferFrom(
+    address from,
+    address to,
+    uint256 tokenId,
+    bytes memory data
+  ) public override(ERC721, IERC721) onlyAllowedOperator(from) {
+    super.safeTransferFrom(from, to, tokenId, data);
+  }
+
+  function setAdmin(address newAdmin) external onlyOwner {
+    require(newAdmin != address(0x0), "invalid address");
+    admin = newAdmin;
+    emit SetAdminRole(newAdmin);
+  }
+
+  function setMinterRole(address minter, bool status) external onlyAdmin {
+    require(minter != address(0x0), "invalid address");
+    isMinter[minter] = status;
+    emit SetMinterRole(minter, status);
+  }
+
+  function setTokenBaseURI(string memory _uri) external onlyOwner {
+    _tokenBaseURI = _uri;
+    emit BaseURIUpdated(_uri);
+  }
+
+  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    require(_exists(tokenId), "BrewlabsIndexNft: URI query for nonexistent token");
+
+    string memory base = _baseURI();
+    string memory description = string(
+      abi.encodePacked(
+        '"description": "',
+        name(),
+        " #",
+        tokenId.toString(),
+        ': Brewlabs Index NFT description"'
+      )
+    );
+
+    IBrewlabsIndex _indexes = IBrewlabsIndex(indexes[tokenId]);
+    uint8 numTokens = _indexes.NUM_TOKENS();
+    (uint256[] memory amounts, uint256 ethAmount) = _indexes.nftInfo(tokenId);
+
+    string memory attributes = '"attributes":[';
+    for (uint8 i = 0; i < numTokens; i++) {
+      address _token = _indexes.tokens(i);
+      if (i > 0) {
+        attributes = string(abi.encodePacked(attributes, ","));
+      }
+
+      attributes = string(
+        abi.encodePacked(
+          attributes,
+          '{"trait_type":"token',
+          uint256(i).toString(),
+          '", "value":"',
+          _token.toHexString(),
+          '"},',
+          '{"trait_type":"amount',
+          uint256(i).toString(),
+          '", "value":"',
+          amounts[i].toString(),
+          '"}'
+        )
+      );
+    }
+    attributes = string(
+      abi.encodePacked(
+        attributes,
+        ', {"trait_type":"zapped amount", "value":"',
+        ethAmount.toString(),
+        '"}]'
+      )
+    );
+
+    // If both are set, concatenate the baseURI (via abi.encodePacked).
+    string memory metadata = string(
+      abi.encodePacked(
+        '{"name": "',
+        name(),
+        '", ',
+        description,
+        ', "image": "',
+        base,
+        '", ',
+        attributes,
+        "}"
+      )
+    );
+
+    return string(abi.encodePacked("data:application/json;base64,", _base64(bytes(metadata))));
+  }
+
+  function getNftInfo(uint256 tokenId) external view returns (uint256[] memory, uint256) {
+    return IBrewlabsIndex(indexes[tokenId]).nftInfo(tokenId);
+  }
+
+  function _baseURI() internal view override returns (string memory) {
+    return _tokenBaseURI;
+  }
+
+  function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
+    require(_exists(tokenId), "BrewlabsIndexNft: URI set of nonexistent token");
+    _tokenURIs[tokenId] = _tokenURI;
+  }
+
+  function _base64(bytes memory data) internal pure returns (string memory) {
+    if (data.length == 0) return "";
+
+    // load the table into memory
+    string memory table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    // multiply by 4/3 rounded up
+    uint256 encodedLen = 4 * ((data.length + 2) / 3);
+
+    // add some extra buffer at the end required for the writing
+    string memory result = new string(encodedLen + 32);
+
+    assembly {
+      // set the actual output length
+      mstore(result, encodedLen)
+
+      // prepare the lookup table
+      let tablePtr := add(table, 1)
+
+      // input ptr
+      let dataPtr := data
+      let endPtr := add(dataPtr, mload(data))
+
+      // result ptr, jump over length
+      let resultPtr := add(result, 32)
+
+      // run over the input, 3 bytes at a time
+      for {
+
+      } lt(dataPtr, endPtr) {
+
+      } {
+        dataPtr := add(dataPtr, 3)
+
+        // read 3 bytes
+        let input := mload(dataPtr)
+
+        // write 4 characters
+        mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(18, input), 0x3F)))))
+        resultPtr := add(resultPtr, 1)
+        mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(12, input), 0x3F)))))
+        resultPtr := add(resultPtr, 1)
+        mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(6, input), 0x3F)))))
+        resultPtr := add(resultPtr, 1)
+        mstore(resultPtr, shl(248, mload(add(tablePtr, and(input, 0x3F)))))
+        resultPtr := add(resultPtr, 1)
+      }
+
+      // padding with '='
+      switch mod(mload(data), 3)
+      case 1 {
+        mstore(sub(resultPtr, 2), shl(240, 0x3d3d))
+      }
+      case 2 {
+        mstore(sub(resultPtr, 1), shl(248, 0x3d))
+      }
+    }
+
+    return result;
+  }
+}
