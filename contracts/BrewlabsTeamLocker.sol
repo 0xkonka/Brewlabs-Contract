8494c1533966bb1c3c4bfb7cd5e59999ea7270bf
brainstormk <109654392+brainstormk@users.noreply.github.com>
2023-07-14 22:23:12 +0800

    Update distribution logic & make test case

--- contracts/BrewlabsTeamLocker.sol
+++ contracts/BrewlabsTeamLocker.sol
@@ -1,83 +1,86 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract BrewlabsTeamLocker is Ownable {
     using SafeERC20 for IERC20;
 
-    address[] public distributors;
+    address[] public members;
     mapping(address => uint256) public rates;
     uint256 public rateDenominator;
 
-    event SetDistributor(address distributor, uint256 position);
-    event RemoveDistribution(address distributor);
-    event Distributed(address distributor, address token, uint256 amount);
+    event SetMember(address member, uint256 rate);
+    event RemoveMember(address member);
+    event Distributed(address member, address token, uint256 amount);
+    event TokenRecovered(address token, uint256 amount);
 
     constructor() {}
 
-    function addDistribution(address distributor, uint256 rate) external onlyOwner {
-        require(rates[distributor] == 0, "already set");
+    function addMember(address member, uint256 rate) external onlyOwner {
+        require(member != address(0x0), "Invalid address");
+        require(rates[member] == 0, "already set");
 
-        distributors.push(distributor);
-        rates[distributor] = rate;
+        members.push(member);
+        rates[member] = rate;
         rateDenominator += rate;
 
-        emit SetDistributor(distributor, rate);
+        emit SetMember(member, rate);
     }
 
-    function removeDistribution(address distributor) external onlyOwner {
-        require(rates[distributor] > 0, "Not found");
+    function removeMember(address member) external onlyOwner {
+        require(rates[member] > 0, "Not found");
 
-        for (uint256 i = 0; i < distributors.length; i++) {
-            if (distributors[i] == distributor) {
-                distributors[i] = distributors[distributors.length - 1];
-                distributors.pop();
+        for (uint256 i = 0; i < members.length; i++) {
+            if (members[i] == member) {
+                members[i] = members[members.length - 1];
+                members.pop();
                 break;
             }
         }
 
-        rateDenominator -= rates[distributor];
-        rates[distributor] = 0;
+        rateDenominator -= rates[member];
+        rates[member] = 0;
 
-        emit RemoveDistribution(distributor);
+        emit RemoveMember(member);
     }
 
-    function updateDistribution(address distributor, uint256 rate) external onlyOwner {
-        require(rates[distributor] > 0, "Not found");
+    function updateMember(address member, uint256 rate) external onlyOwner {
+        require(rates[member] > 0, "Not found");
 
-        rateDenominator = rateDenominator - rates[distributor] + rate;
-        rates[distributor] = rate;
+        rateDenominator = rateDenominator - rates[member] + rate;
+        rates[member] = rate;
 
-        emit SetDistributor(distributor, rate);
+        emit SetMember(member, rate);
     }
 
-    function numDistributors() external view returns (uint256) {
-        return distributors.length;
+    function numMembers() external view returns (uint256) {
+        return members.length;
     }
 
-    function rateOfDistributor(address distributor) external view returns (uint256 numerator, uint256 denominator) {
-        return (rates[distributor], rateDenominator);
+    function rateOfMember(address member) external view returns (uint256 numerator, uint256 denominator) {
+        return (rates[member], rateDenominator);
     }
 
     function distribute(address[] memory tokens) external onlyOwner {
+        require(tokens.length > 0, "wrong config");
         for (uint256 i = 0; i < tokens.length; i++) {
             _distributeTokens(tokens[i]);
         }
     }
 
     function _distributeTokens(address token) internal {
-        uint256 amount;
+        uint256 balance;
         if (token == address(0x0)) {
-            amount = address(this).balance;
+            balance = address(this).balance;
         } else {
-            amount = IERC20(token).balanceOf(address(this));
+            balance = IERC20(token).balanceOf(address(this));
         }
 
-        for (uint256 i = 0; i < distributors.length; i++) {
-            address user = distributors[i];
-            uint256 _amount = amount * rates[user] / rateDenominator;
+        for (uint256 i = 0; i < members.length; i++) {
+            address user = members[i];
+            uint256 _amount = balance * rates[user] / rateDenominator;
             _transferToken(token, user, _amount);
 
             emit Distributed(user, token, _amount);
@@ -90,6 +93,27 @@ contract BrewlabsTeamLocker is Ownable {
         } else {
             IERC20(token).safeTransfer(to, amount);
         }
+    }
+
+    /**
+     * @notice It allows the owner to recover wrong tokens sent to the contract
+     * @param _token: the address of the token to withdraw
+     * @param _amount: withdrawal amount; if _amount is zero, withdrawal amount is equal to token balance
+     * @dev This function is only callable by owner.
+     */
+    function rescueTokens(address _token, uint256 _amount) external onlyOwner {
+        uint256 balance;
+        if (_token == address(0x0)) {
+            balance = address(this).balance;
+        } else {
+            balance = IERC20(_token).balanceOf(address(this));
+        }
+        if (_amount == 0) _amount = balance;
+
+        require(balance >= _amount, "Insufficient balance");
+
+        _transferToken(_token, msg.sender, _amount);
+        emit TokenRecovered(_token, _amount);
     }
 
     receive() external payable {}
---
+++ test/forge/BrewlabsTeamLocker.t.sol
@@ -0,0 +1,267 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+
+import {BrewlabsTeamLocker, IERC20} from "../../contracts/BrewlabsTeamLocker.sol";
+import {MockErc20} from "../../contracts/mocks/MockErc20.sol";
+import {Utils} from "./utils/Utils.sol";
+
+contract BrewlabsTeamLockerTest is Test {
+    BrewlabsTeamLocker internal locker;
+    Utils internal utils;
+
+    event SetMember(address member, uint256 rate);
+    event RemoveMember(address member);
+    event Distributed(address member, address token, uint256 amount);
+
+    function setUp() public {
+        utils = new Utils();
+        locker = new BrewlabsTeamLocker();
+    }
+
+    function test_addMember() public {
+        vm.expectEmit(true, true, true, true);
+        emit SetMember(address(0x1), 30);
+        locker.addMember(address(0x1), 30);
+
+        (uint256 numerator, uint256 denominator) = locker.rateOfMember(address(0x1));
+        assertEq(locker.numMembers(), 1);
+        assertEq(numerator, 30);
+        assertEq(denominator, 30);
+
+        for (uint160 i = 0; i < 3; i++) {
+            locker.addMember(address(i + 2), 30);
+        }
+
+        (numerator, denominator) = locker.rateOfMember(address(0x3));
+        assertEq(locker.numMembers(), 4);
+        assertEq(numerator, 30);
+        assertEq(denominator, 120);
+    }
+
+    function test_addMemberFailedForInvalidAddressAndDuplicatedMember() public {
+        locker.addMember(address(0x1), 30);
+
+        vm.expectRevert("already set");
+        locker.addMember(address(0x1), 20);
+
+        vm.expectRevert("Invalid address");
+        locker.addMember(address(0x0), 20);
+    }
+
+    function tryAddMembers() internal {
+        locker.addMember(address(0x12345), 30);
+        locker.addMember(address(0x12346), 30);
+        locker.addMember(address(0x12347), 20);
+        locker.addMember(address(0x12348), 20);
+    }
+
+    function test_removeMember() public {
+        tryAddMembers();
+
+        vm.expectRevert("Not found");
+        locker.removeMember(address(0x1));
+
+        vm.expectEmit(true, true, true, true);
+        emit RemoveMember(address(0x12346));
+        locker.removeMember(address(0x12346));
+
+        assertEq(locker.numMembers(), 3);
+
+        (uint256 numerator, uint256 denominator) = locker.rateOfMember(address(0x12345));
+        assertEq(numerator, 30);
+        assertEq(denominator, 70);
+
+        (numerator, denominator) = locker.rateOfMember(address(0x12346));
+        assertEq(numerator, 0);
+        assertEq(denominator, 70);
+
+        assertEq(locker.members(1), address(0x12348));
+
+        vm.expectRevert("Not found");
+        locker.removeMember(address(0x12346));
+
+        locker.removeMember(address(0x12345));
+        locker.removeMember(address(0x12347));
+        locker.removeMember(address(0x12348));
+        assertEq(locker.rateDenominator(), 0);
+    }
+
+    function test_updateMember() public {
+        tryAddMembers();
+
+        vm.expectRevert("Not found");
+        locker.updateMember(address(0x1), 10);
+
+        vm.expectEmit(true, true, true, true);
+        emit SetMember(address(0x12347), 50);
+        locker.updateMember(address(0x12347), 50);
+
+        assertEq(locker.numMembers(), 4);
+
+        (uint256 numerator, uint256 denominator) = locker.rateOfMember(address(0x12347));
+        assertEq(numerator, 50);
+        assertEq(denominator, 130);
+
+        locker.updateMember(address(0x12347), 10);
+        (numerator, denominator) = locker.rateOfMember(address(0x12347));
+        assertEq(numerator, 10);
+        assertEq(denominator, 90);
+    }
+
+    function test_distributeSingleToken() public {
+        tryAddMembers();
+
+        MockErc20 token = new MockErc20(18);
+        token.mint(address(locker), 30 ether);
+
+        address[] memory tokens;
+        vm.expectRevert("wrong config");
+        locker.distribute(tokens);
+
+        tokens = new address[](1);
+        tokens[0] = address(token);
+        locker.distribute(tokens);
+
+        uint256 remained = 30 ether;
+        for (uint160 i = 0; i < 4; i++) {
+            address member = address(0x12345 + i);
+            (uint256 numerator, uint256 denominator) = locker.rateOfMember(member);
+
+            uint256 amount = 30 ether * numerator / denominator;
+            assertEq(token.balanceOf(member), amount);
+            remained -= amount;
+        }
+
+        assertEq(token.balanceOf(address(locker)), remained);
+    }
+
+    function test_distributeETH() public {
+        tryAddMembers();
+
+        vm.deal(address(locker), 0.3 ether);
+
+        address[] memory tokens;
+        tokens = new address[](1);
+        tokens[0] = address(0x0);
+        locker.distribute(tokens);
+
+        uint256 remained = 0.3 ether;
+        for (uint160 i = 0; i < 4; i++) {
+            address member = address(0x12345 + i);
+            (uint256 numerator, uint256 denominator) = locker.rateOfMember(member);
+
+            uint256 amount = 0.3 ether * numerator / denominator;
+            assertEq(address(member).balance, amount);
+            remained -= amount;
+        }
+
+        assertEq(address(locker).balance, remained);
+    }
+
+    function test_distributeMultipleTokens() public {
+        tryAddMembers();
+
+        vm.deal(address(locker), 0.3 ether);
+
+        MockErc20 _token;
+        address[] memory tokens = new address[](10);
+        uint256[] memory amounts = new uint256[](10);
+        amounts[5] = 0.3 ether;
+        for (uint256 i = 0; i < 10; i++) {
+            if (i == 5) continue;
+
+            if (i % 2 == 0) {
+                _token = new MockErc20(9);
+                amounts[i] = 13 gwei * (i + 3);
+            } else {
+                _token = new MockErc20(18);
+                amounts[i] = 1.3 ether * (i + 7);
+            }
+            _token.mint(address(locker), amounts[i]);
+
+            tokens[i] = address(_token);
+        }
+
+        locker.distribute(tokens);
+
+        for (uint256 k = 0; k < 10; k++) {
+            address token = tokens[k];
+            uint256 balance = amounts[k];
+
+            uint256 remained = balance;
+            for (uint160 i = 0; i < 4; i++) {
+                address member = address(0x12345 + i);
+                (uint256 numerator, uint256 denominator) = locker.rateOfMember(member);
+
+                uint256 amount = balance * numerator / denominator;
+                if (token == address(0x0)) {
+                    assertEq(address(member).balance, amount);
+                } else {
+                    assertEq(IERC20(token).balanceOf(address(member)), amount);
+                }
+                remained -= amount;
+            }
+
+            if (token == address(0x0)) {
+                assertEq(address(locker).balance, remained);
+            } else {
+                assertEq(IERC20(token).balanceOf(address(locker)), remained);
+            }
+        }
+    }
+
+    function test_rescueTokensForEther() public {
+        uint256 beforeBal = address(locker.owner()).balance;
+
+        vm.deal(address(locker), 0.02 ether);
+        locker.rescueTokens(address(0x0), 0);
+        assertEq(address(locker).balance, 0);
+
+        uint256 afterBal = address(locker.owner()).balance;
+        assertEq(afterBal - beforeBal, 0.02 ether);
+    }
+
+    function test_rescueTokensForErc20() public {
+        MockErc20 token = new MockErc20(18);
+        token.mint(address(locker), 1000 ether);
+        locker.rescueTokens(address(token), 0);
+        assertEq(token.balanceOf(address(locker)), 0);
+        assertEq(token.balanceOf(locker.owner()), 1000 ether);
+    }
+
+    function test_rescueTokensForSome() public {
+        MockErc20 token = new MockErc20(18);
+        token.mint(address(locker), 1000 ether);
+
+        vm.expectRevert("Insufficient balance");
+        locker.rescueTokens(address(token), 1700 ether);
+
+        locker.rescueTokens(address(token), 700 ether);
+        assertEq(token.balanceOf(address(locker)), 300 ether);
+        assertEq(token.balanceOf(locker.owner()), 700 ether);
+    }
+
+    function test_rescueTokensAsOwner() public {
+        address owner = locker.owner();
+        uint256 prevBalance = owner.balance;
+
+        vm.deal(address(locker), 0.02 ether);
+        locker.rescueTokens(address(0x0), 0);
+
+        assertEq(owner.balance, prevBalance + 0.02 ether);
+    }
+
+    function test_rescueTokensFailsAsNotOwner() public {
+        vm.startPrank(address(0x1));
+
+        vm.deal(address(locker), 0.02 ether);
+        vm.expectRevert("Ownable: caller is not the owner");
+        locker.rescueTokens(address(0x0), 0);
+
+        vm.stopPrank();
+    }
+
+    receive() external payable {}
+}
