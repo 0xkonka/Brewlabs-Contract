5538a7b30190acfcd50a4002093030091d84178d
chainpioneer1 <pandaworld1985@outlook.com>
2024-02-26 22:19:51 +0800

    add list and buy

--- contracts/marketplace/BrewsMarketplace.sol
+++ contracts/marketplace/BrewsMarketplace.sol
@@ -1,26 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import {IERC20MetadataUpgradeable, IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
+import {SafeERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
-contract BrewlabsMarketplace is OwnableUpgradeable {
-    using SafeERC20 for IERC20;
-
-    struct TokenGroup {
-        // token addresses
-        address[] addresses;
-        bytes32 groupName;
-    }
+contract BrewsMarketplace is OwnableUpgradeable, ReentrancyGuardUpgradeable {
+    using SafeERC20Upgradeable for IERC20Upgradeable;
 
     struct MarketInfo {
-        // listed token address
+        // token address listed on market
         address sellToken;
         // vesting time
         uint96 vestingTime;
-        // seller address
-        address seller;
-        uint96 startTime;
+        // vendor address
+        address vendor;
+        // listed time
+        uint96 listedTime;
         // listed price, precision 18
         uint256 price;
         // sell amount
@@ -29,17 +26,22 @@ contract BrewlabsMarketplace is OwnableUpgradeable {
         uint256 reserve;
         // purchase count
         uint256 purchaseCount;
+        // paid token address
+        address paidToken;
     }
 
     struct Purchase {
         address buyer;
         uint96 boughtTime;
+        // sell token amount
         uint256 buyAmount;
         uint256 claimed;
+        // token amount paid to purchase
+        uint256 paidTokenAmount;
+        uint256 claimedPaidToken;
     }
 
     // variables
-    uint32[] vestingPeriods;
     uint256 marketCount;
     // market info by address
     mapping(uint256 => MarketInfo) markets;
@@ -47,55 +49,120 @@ contract BrewlabsMarketplace is OwnableUpgradeable {
     mapping(uint256 => mapping(uint256 => Purchase)) purchases;
     // minimum amounts by token address
     mapping(address => uint256) minAmounts;
+    // whitelist for sell tokens
+    mapping(address => bool) bSellTokens;
+    // whitelist for tokens paid to purchase
+    mapping(address => bool) bPaidTokens;
+    // limit for vestingTimes;
+    uint256 internal _maxVestingDays;
+    uint256 internal _performanceFee;
+    address internal _treasury;
+    uint256 internal _purchaseFee;
+    uint256 constant PERCENT_PRECISION = 10000;
+    // events
+    event ListEvent(MarketInfo market);
+    event PurchaseEvent(Purchase purchase);
 
     constructor() {}
 
     function initialize() external initializer {
         __Ownable_init();
-        vestingPeriods = [0, 2 days, 3 days, 5 days];
+        __ReentrancyGuard_init();
+        _maxVestingDays = 730;
+        _performanceFee = 0.0035 ether;
+        _treasury = 0x5Ac58191F3BBDF6D037C6C6201aDC9F99c93C53A;
+        _purchaseFee = 30;
     }
 
-    function listToken(MarketInfo memory marketInfo) external payable {
-        require(marketInfo.sellToken != address(0), "invalid token");
-        require(marketInfo.price != 0, "invalid price");
-        require(
-            marketInfo.sellAmount >= minAmounts[marketInfo.sellToken],
-            "invalid price"
-        );
-        marketCount++;
-        marketInfo.startTime = uint96(block.timestamp);
-        marketInfo.seller = msg.sender;
-        marketInfo.purchaseCount = 0;
-        markets[marketCount] = marketInfo;
+    function listToken(
+        address sellToken,
+        uint256 vestingDays,
+        uint256 price,
+        uint256 sellAmount,
+        address paidToken
+    ) external payable nonReentrant {
+        require(price != 0, "invalid price");
+        require(sellAmount >= minAmounts[sellToken], "invalid amount");
+        require(bSellTokens[sellToken], "invalid sell token");
+        require(bPaidTokens[paidToken], "invalid paid token");
+        require(vestingDays <= _maxVestingDays, "invalid vesting days");
+        _transferPerformanceFee();
 
-        IERC20(marketInfo.sellToken).safeTransferFrom(
+        marketCount++;
+        MarketInfo memory m = MarketInfo({
+            sellToken: sellToken,
+            vestingTime: uint96(vestingDays * 1 days),
+            price: price,
+            sellAmount: sellAmount,
+            paidToken: paidToken,
+            vendor: msg.sender,
+            listedTime: uint96(block.timestamp),
+            reserve: sellAmount,
+            purchaseCount: 0
+        });
+        markets[marketCount] = m;
+
+        IERC20Upgradeable(sellToken).safeTransferFrom(
             msg.sender,
             address(this),
-            marketInfo.sellAmount
+            sellAmount
         );
+        emit ListEvent(m);
     }
 
-    function purchase(uint256 marketId, uint256 amount) external {
+    function purchase(
+        uint256 marketId,
+        uint256 amount
+    ) external payable nonReentrant {
+        _transferPerformanceFee();
         MarketInfo memory market = markets[marketId];
         require(market.reserve >= amount, "insufficient listed amount");
         markets[marketId].reserve -= amount;
-
+        uint256 denominator = (10 **
+            (18 -
+                IERC20MetadataUpgradeable(market.paidToken).decimals() +
+                IERC20MetadataUpgradeable(market.sellToken).decimals()));
+        uint256 paidTokenAmount = (amount * market.price) / denominator;
+        uint256 fee = (_purchaseFee * paidTokenAmount) / PERCENT_PRECISION;
+        paidTokenAmount -= fee;
+        IERC20Upgradeable(market.paidToken).safeTransferFrom(
+            msg.sender,
+            address(this),
+            paidTokenAmount
+        );
+        IERC20Upgradeable(market.paidToken).safeTransferFrom(
+            msg.sender,
+            _treasury,
+            fee
+        );
         Purchase memory p = Purchase({
             buyer: msg.sender,
             buyAmount: amount,
             claimed: 0,
-            boughtTime: uint96(block.timestamp)
+            boughtTime: uint96(block.timestamp),
+            paidTokenAmount: paidTokenAmount,
+            claimedPaidToken: 0
         });
         ++markets[marketId].purchaseCount;
         purchases[marketId][markets[marketId].purchaseCount] = p;
+        emit PurchaseEvent(p);
     }
 
-    function claimPurchase(uint256 marketId, uint256 purchaseId) external {
+    /**
+     * @notice buyer claim purchased token
+     * @param marketId market id
+     * @param purchaseId purchase id
+     */
+    function claimPurchase(
+        uint256 marketId,
+        uint256 purchaseId
+    ) external payable nonReentrant {
+        _transferPerformanceFee();
         Purchase memory p = purchases[marketId][purchaseId];
         MarketInfo memory m = markets[marketId];
         require(p.buyer == msg.sender, "invalid buyer");
         uint256 claimable;
-        if (p.boughtTime + m.vestingTime >= block.timestamp) {
+        if (p.boughtTime + m.vestingTime <= block.timestamp) {
             claimable = p.buyAmount - p.claimed;
         } else {
             claimable =
@@ -103,21 +170,77 @@ contract BrewlabsMarketplace is OwnableUpgradeable {
                 m.vestingTime -
                 p.claimed;
         }
-        IERC20(m.sellToken).safeTransferFrom(
-        msg.sender,
-            address(this),
-            claimable
-        );
+        IERC20Upgradeable(m.sellToken).safeTransfer(msg.sender, claimable);
         purchases[marketId][purchaseId].claimed = p.claimed + claimable;
-    }    
+    }
 
-    // check if address is contract
-    function isContract(address _addr) internal view returns (bool) {
-        uint32 size;
-        assembly {
-            size := extcodesize(_addr)
+    function claimPaidToken(
+        uint256 marketId,
+        uint256 purchaseId
+    ) external payable nonReentrant {
+        _transferPerformanceFee();
+        Purchase memory p = purchases[marketId][purchaseId];
+        MarketInfo memory m = markets[marketId];
+        require(m.vendor == msg.sender, "invalid vendor");
+        uint256 claimable;
+        if (p.boughtTime + m.vestingTime <= block.timestamp) {
+            claimable = p.paidTokenAmount - p.claimedPaidToken;
+        } else {
+            claimable =
+                (p.paidTokenAmount * (block.timestamp - p.boughtTime)) /
+                m.vestingTime -
+                p.claimedPaidToken;
         }
-        return (size > 0);
+        IERC20Upgradeable(m.paidToken).safeTransfer(msg.sender, claimable);
+        purchases[marketId][purchaseId].claimedPaidToken =
+            p.claimedPaidToken +
+            claimable;
+    }
+
+    function getMarket(
+        uint256 marketId
+    ) external view returns (MarketInfo memory) {
+        return markets[marketId];
+    }
+
+    function getPurchase(
+        uint256 marketId,
+        uint256 purchaseId
+    ) external view returns (Purchase memory) {
+        return purchases[marketId][purchaseId];
+    }
+
+    function enableSellTokens(
+        address[] calldata sellTokens,
+        bool bEnable
+    ) external onlyOwner {
+        for (uint256 i; i < sellTokens.length; ) {
+            bSellTokens[sellTokens[i]] = bEnable;
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function enablePaidTokens(
+        address[] calldata paidTokens,
+        bool bEnable
+    ) external onlyOwner {
+        for (uint256 i; i < paidTokens.length; ) {
+            bPaidTokens[paidTokens[i]] = bEnable;
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function _transferPerformanceFee() internal {
+        require(msg.value >= _performanceFee, "should pay small gas");
+
+        payable(_treasury).transfer(_performanceFee);
+        if (msg.value > _performanceFee) {
+            payable(msg.sender).transfer(msg.value - _performanceFee);
+        }
     }
 
     /**
@@ -125,14 +248,20 @@ contract BrewlabsMarketplace is OwnableUpgradeable {
      * @param _token: token address
      */
     function rescueTokens(address _token) external onlyOwner {
+        require(
+            !bSellTokens[_token] && !bPaidTokens[_token],
+            "can't be sell or paid tokens"
+        );
         if (_token == address(0x0)) {
             uint256 _ethAmount = address(this).balance;
             payable(msg.sender).transfer(_ethAmount);
         } else {
-            uint256 _tokenAmount = IERC20(_token).balanceOf(address(this));
-            IERC20(_token).safeTransfer(msg.sender, _tokenAmount);
+            uint256 _tokenAmount = IERC20Upgradeable(_token).balanceOf(
+                address(this)
+            );
+            IERC20Upgradeable(_token).safeTransfer(msg.sender, _tokenAmount);
         }
     }
-    
+
     receive() external payable {}
 }
---
+++ test/forge/marketplace/BrewsMarketplace.t.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+
+import {BrewsMarketplace} from "../../../contracts/marketplace/BrewsMarketplace.sol";
+import {MockErc20} from "../../../contracts/mocks/MockErc20.sol";
+
+import {Utils} from "../utils/Utils.sol";
+
+contract BrewsMarketplaceTest is Test {
+    address internal BREWLABS = 0x6aAc56305825f712Fd44599E59f2EdE51d42C3e7;
+    address internal USDC = 0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d;
+    address internal USDT = 0x55d398326f99059fF775485246999027B3197955;
+    address internal DAI = 0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3;
+
+    address internal vendor = address(0x111);
+    address internal buyer1 = address(0x333);
+    address internal buyer2 = address(0x444);
+    address internal deployer = address(0x123);
+
+    BrewsMarketplace internal marketplace;
+    MockErc20 internal sellToken;
+    MockErc20 internal paidToken1;
+    MockErc20 internal paidToken2;
+    Utils internal utils;
+
+    uint256 mainnetFork;
+    string MAINNET_RPC_URL = "https://bsc-dataseed.binance.org/";
+
+    function setUp() public {
+        mainnetFork = vm.createFork(MAINNET_RPC_URL);
+        vm.selectFork(mainnetFork);
+
+        utils = new Utils();
+
+        vm.startPrank(deployer);
+        marketplace = new BrewsMarketplace();
+        marketplace.initialize();
+
+        sellToken = new MockErc20(18);
+        paidToken1 = new MockErc20(18);
+        paidToken2 = new MockErc20(6);
+        sellToken.mint(vendor, 100 * 1e18);
+        paidToken1.mint(buyer1, 100 * 1e18);
+        paidToken2.mint(buyer2, 100 * 1e6);
+        address[] memory sellTokens = new address[](1);
+        sellTokens[0] = address(sellToken);
+        marketplace.enableSellTokens(sellTokens, true);
+        address[] memory paidTokens = new address[](2);
+        paidTokens[0] = address(paidToken1);
+        paidTokens[1] = address(paidToken2);
+        marketplace.enablePaidTokens(paidTokens, true);
+        vm.stopPrank();
+    }
+
+    function tryList(
+        address token,
+        address paidToken,
+        uint256 amount,
+        uint256 price
+    ) internal {
+        marketplace.listToken{value: 0.0035 ether}(
+            token,
+            2,
+            price,
+            amount,
+            paidToken
+        );
+    }
+
+    function test_listToken() public {
+        vm.deal(vendor, 1 ether);
+        vm.startPrank(vendor);
+        sellToken.approve(address(marketplace), 10 * 1e18);
+        tryList(address(sellToken), address(paidToken1), 10 * 1e18, 2 * 1e18);
+        assertEq(sellToken.balanceOf(address(marketplace)), 10 * 1e18);
+        vm.stopPrank();
+        vm.startPrank(buyer1);
+        vm.deal(buyer1, 1 ether);
+        paidToken1.approve(address(marketplace), 20 * 1e18);
+        marketplace.purchase{value: 0.0035 ether}(1, 10 * 1e18);
+        assertEq(
+            paidToken1.balanceOf(address(marketplace)),
+            (10 * 2 * 1e18 * (1000 - 3)) / 1000
+        );
+        skip(12 hours);
+        marketplace.claimPurchase{value: 0.0035 ether}(1, 1);
+        assertEq(sellToken.balanceOf(address(marketplace)), (75 * 1e17));
+        // Claim stable coin for vendor
+        vm.stopPrank();
+        vm.startPrank(vendor);
+        marketplace.claimPaidToken{value: 0.0035 ether}(1, 1);
+        vm.stopPrank();
+
+        vm.startPrank(buyer1);
+        skip(12 hours);
+        marketplace.claimPurchase{value: 0.0035 ether}(1, 1);
+        assertEq(sellToken.balanceOf(address(marketplace)), (5 * 1e18));
+    }
+
+    receive() external payable {}
+}
