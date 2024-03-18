5417e99465047ef348773ce6104bc588aee061c6
letteldream <letteldream@gmail.com>
2024-03-18 02:42:07 -0500

    feat: add staking pool contract & staking factory contract

--- contracts/pool/BrewlabsStakingFactory.sol
+++ contracts/pool/BrewlabsStakingFactory.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
 import "./BrewlabsStaking.sol";
-import "./interfaces/IBrewlabsStaking.sol";
+import "./IBrewlabsStaking.sol";
 
 contract BrewlabsStakingFactory {
     bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(BrewlabsStaking).creationCode));
